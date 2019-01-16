31319ba0ae82ff34ab860a1ec04de970359f45bd
bushra <yousaf655@hotmail.com>
2019-01-16 10:47:31 -0700

    Added routes

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,25 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
+});
+
+Route::get('about', function () {
+    return view('about');
+});
+
+Route::get('home', function () {
+    return view('home');
+});
+
+Route::get('signup', function () {
+    return view('signup');
+});
+
+Route::get('posts', function () {
+    return view('posts');
+});
+
+Route::get('login', function () {
+    return view('login');
 });
